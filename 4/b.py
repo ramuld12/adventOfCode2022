83bce0bf2c025d3575d3f831053863233bbf5a1f
ramuld12 <rasmus.herskind@hotmail.com>
2022-12-06 16:44:09 +0100

    Day 4

---
+++ 4/b.py
@@ -0,0 +1,20 @@
+
+
+def elf_list_to_set(elf_list):
+    elf_set = set()
+    for i in range(elf_list[0], elf_list[1] + 1):
+        elf_set.add(i)
+    return elf_set
+
+
+if __name__ == '__main__':
+    f = open("input.txt", "r")
+    sum = 0
+    while line := f.readline().strip("\n"):
+        elf_left, elf_right = [list(map(int, i.split("-"))) for i in line.split(",")]
+        elf_left = elf_list_to_set(elf_left)
+        elf_right = elf_list_to_set(elf_right)
+        if len(elf_left.intersection(elf_right)) != 0:
+            sum += 1
+    print(sum)
+    f.close()
